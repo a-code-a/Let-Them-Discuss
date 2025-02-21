ae1cc8f1259d76d49a37065654e8ee47fa43f525
ASee <55654420+a-code-a@users.noreply.github.com>
2025-02-21 11:13:15 +0100

    info

--- src/components/ModeratorPanel/ModeratorPanel.js
+++ src/components/ModeratorPanel/ModeratorPanel.js
@@ -29,6 +29,15 @@ const ModeratorPanel = ({
         >
           {isDiscussionActive ? 'Diskussion beenden' : 'Diskussion starten'}
         </button>
+        <div className="instruction">
+          <h4>Anleitung:</h4>
+          <p>
+            Ziehe Personas per Drag & Drop in das Chatfenster, um ihnen Fragen zu stellen.<br/><br/>
+            Du kannst eine oder mehrere Personas auswählen.<br/><br/>
+            Wenn mehrere Personas ausgewählt sind, kannst du eine Diskussion starten.<br/><br/>
+            Feedback ist über das Hamburgermenü erwünscht.
+          </p>
+        </div>
       </div>
 
       {topic && (
